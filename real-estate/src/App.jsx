b3ebec08b83b9445113de6a62a364c429bf5e166
suyash <suyash08cr7@gmail.com>
2024-07-27 19:16:40 +0530

    new

---
+++ real-estate/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes,
+  Route
+ } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-out' element={<SignOut/>} />
+        <Route path='/profile' element={<Profile/>} />
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
